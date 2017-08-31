3814edd24db49cf561d36812a93034e614e78133
JKoppel93 <JKoppel93@users.noreply.github.com>
2017-08-31 00:23:50 -0400

    updates

---
+++ views/css/bootstrap-grid.js
@@ -0,0 +1,602 @@
+import React, {StyleSheet, Dimensions, PixelRatio} from "react-native";
+const {width, height, scale} = Dimensions.get("window"),
+    vw = width / 100,
+    vh = height / 100,
+    vmin = Math.min(vw, vh),
+    vmax = Math.max(vw, vh);
+
+export default StyleSheet.create({
+    "img": {
+        "verticalAlign": "middle"
+    },
+    "img-responsive": {
+        "display": "block",
+        "width": "100% \\9",
+        "maxWidth": "100%",
+        "height": "auto"
+    },
+    "img-rounded": {
+        "borderRadius": 6
+    },
+    "img-thumbnail": {
+        "paddingTop": 4,
+        "paddingRight": 4,
+        "paddingBottom": 4,
+        "paddingLeft": 4,
+        "lineHeight": 1.42857143,
+        "backgroundColor": "#ffffff",
+        "border": "1px solid #dddddd",
+        "borderRadius": 4,
+        "WebkitTransition": "all 0.2s ease-in-out",
+        "OTransition": "all 0.2s ease-in-out",
+        "transition": "all 0.2s ease-in-out",
+        "display": "inline-block",
+        "width": "100% \\9",
+        "maxWidth": "100%",
+        "height": "auto"
+    },
+    "img-circle": {
+        "borderRadius": "50%"
+    },
+    "hr": {
+        "marginTop": 20,
+        "marginBottom": 20,
+        "border": 0,
+        "borderTop": "1px solid #eeeeee"
+    },
+    "sr-only": {
+        "position": "absolute",
+        "width": 1,
+        "height": 1,
+        "marginTop": -1,
+        "marginRight": -1,
+        "marginBottom": -1,
+        "marginLeft": -1,
+        "paddingTop": 0,
+        "paddingRight": 0,
+        "paddingBottom": 0,
+        "paddingLeft": 0,
+        "overflow": "hidden",
+        "clip": "rect(0, 0, 0, 0)",
+        "border": 0
+    },
+    "sr-only-focusable:active": {
+        "position": "static",
+        "width": "auto",
+        "height": "auto",
+        "marginTop": 0,
+        "marginRight": 0,
+        "marginBottom": 0,
+        "marginLeft": 0,
+        "overflow": "visible",
+        "clip": "auto"
+    },
+    "sr-only-focusable:focus": {
+        "position": "static",
+        "width": "auto",
+        "height": "auto",
+        "marginTop": 0,
+        "marginRight": 0,
+        "marginBottom": 0,
+        "marginLeft": 0,
+        "overflow": "visible",
+        "clip": "auto"
+    },
+    "container": {
+        "marginRight": "auto",
+        "marginLeft": "auto",
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "container-fluid": {
+        "marginRight": "auto",
+        "marginLeft": "auto",
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "row": {
+        "marginLeft": -15,
+        "marginRight": -15
+    },
+    "col-xs-1": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15,
+        "float": "left",
+        "width": "8.33333333%"
+    },
+    "col-sm-1": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-md-1": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-lg-1": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-xs-2": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15,
+        "float": "left",
+        "width": "16.66666667%"
+    },
+    "col-sm-2": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-md-2": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-lg-2": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-xs-3": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15,
+        "float": "left",
+        "width": "25%"
+    },
+    "col-sm-3": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-md-3": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-lg-3": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-xs-4": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15,
+        "float": "left",
+        "width": "33.33333333%"
+    },
+    "col-sm-4": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-md-4": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-lg-4": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-xs-5": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15,
+        "float": "left",
+        "width": "41.66666667%"
+    },
+    "col-sm-5": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-md-5": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-lg-5": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-xs-6": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15,
+        "float": "left",
+        "width": "50%"
+    },
+    "col-sm-6": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-md-6": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-lg-6": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-xs-7": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15,
+        "float": "left",
+        "width": "58.33333333%"
+    },
+    "col-sm-7": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-md-7": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-lg-7": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-xs-8": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15,
+        "float": "left",
+        "width": "66.66666667%"
+    },
+    "col-sm-8": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-md-8": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-lg-8": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-xs-9": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15,
+        "float": "left",
+        "width": "75%"
+    },
+    "col-sm-9": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-md-9": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-lg-9": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-xs-10": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15,
+        "float": "left",
+        "width": "83.33333333%"
+    },
+    "col-sm-10": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-md-10": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-lg-10": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-xs-11": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15,
+        "float": "left",
+        "width": "91.66666667%"
+    },
+    "col-sm-11": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-md-11": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-lg-11": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-xs-12": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15,
+        "float": "left",
+        "width": "100%"
+    },
+    "col-sm-12": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-md-12": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-lg-12": {
+        "position": "relative",
+        "minHeight": 1,
+        "paddingLeft": 15,
+        "paddingRight": 15
+    },
+    "col-xs-pull-12": {
+        "right": "100%"
+    },
+    "col-xs-pull-11": {
+        "right": "91.66666667%"
+    },
+    "col-xs-pull-10": {
+        "right": "83.33333333%"
+    },
+    "col-xs-pull-9": {
+        "right": "75%"
+    },
+    "col-xs-pull-8": {
+        "right": "66.66666667%"
+    },
+    "col-xs-pull-7": {
+        "right": "58.33333333%"
+    },
+    "col-xs-pull-6": {
+        "right": "50%"
+    },
+    "col-xs-pull-5": {
+        "right": "41.66666667%"
+    },
+    "col-xs-pull-4": {
+        "right": "33.33333333%"
+    },
+    "col-xs-pull-3": {
+        "right": "25%"
+    },
+    "col-xs-pull-2": {
+        "right": "16.66666667%"
+    },
+    "col-xs-pull-1": {
+        "right": "8.33333333%"
+    },
+    "col-xs-pull-0": {
+        "right": "auto"
+    },
+    "col-xs-push-12": {
+        "left": "100%"
+    },
+    "col-xs-push-11": {
+        "left": "91.66666667%"
+    },
+    "col-xs-push-10": {
+        "left": "83.33333333%"
+    },
+    "col-xs-push-9": {
+        "left": "75%"
+    },
+    "col-xs-push-8": {
+        "left": "66.66666667%"
+    },
+    "col-xs-push-7": {
+        "left": "58.33333333%"
+    },
+    "col-xs-push-6": {
+        "left": "50%"
+    },
+    "col-xs-push-5": {
+        "left": "41.66666667%"
+    },
+    "col-xs-push-4": {
+        "left": "33.33333333%"
+    },
+    "col-xs-push-3": {
+        "left": "25%"
+    },
+    "col-xs-push-2": {
+        "left": "16.66666667%"
+    },
+    "col-xs-push-1": {
+        "left": "8.33333333%"
+    },
+    "col-xs-push-0": {
+        "left": "auto"
+    },
+    "col-xs-offset-12": {
+        "marginLeft": "100%"
+    },
+    "col-xs-offset-11": {
+        "marginLeft": "91.66666667%"
+    },
+    "col-xs-offset-10": {
+        "marginLeft": "83.33333333%"
+    },
+    "col-xs-offset-9": {
+        "marginLeft": "75%"
+    },
+    "col-xs-offset-8": {
+        "marginLeft": "66.66666667%"
+    },
+    "col-xs-offset-7": {
+        "marginLeft": "58.33333333%"
+    },
+    "col-xs-offset-6": {
+        "marginLeft": "50%"
+    },
+    "col-xs-offset-5": {
+        "marginLeft": "41.66666667%"
+    },
+    "col-xs-offset-4": {
+        "marginLeft": "33.33333333%"
+    },
+    "col-xs-offset-3": {
+        "marginLeft": "25%"
+    },
+    "col-xs-offset-2": {
+        "marginLeft": "16.66666667%"
+    },
+    "col-xs-offset-1": {
+        "marginLeft": "8.33333333%"
+    },
+    "col-xs-offset-0": {
+        "marginLeft": "0%"
+    },
+    "clearfix:before": {
+        "content": " ",
+        "display": "table"
+    },
+    "clearfix:after": {
+        "content": " ",
+        "display": "table",
+        "clear": "both"
+    },
+    "container:before": {
+        "content": " ",
+        "display": "table"
+    },
+    "container:after": {
+        "content": " ",
+        "display": "table",
+        "clear": "both"
+    },
+    "container-fluid:before": {
+        "content": " ",
+        "display": "table"
+    },
+    "container-fluid:after": {
+        "content": " ",
+        "display": "table",
+        "clear": "both"
+    },
+    "row:before": {
+        "content": " ",
+        "display": "table"
+    },
+    "row:after": {
+        "content": " ",
+        "display": "table",
+        "clear": "both"
+    },
+    "center-block": {
+        "display": "block",
+        "marginLeft": "auto",
+        "marginRight": "auto"
+    },
+    "pull-right": {
+        "float": "right !important"
+    },
+    "pull-left": {
+        "float": "left !important"
+    },
+    "hide": {
+        "display": "none !important"
+    },
+    "show": {
+        "display": "block !important"
+    },
+    "invisible": {
+        "visibility": "hidden"
+    },
+    "text-hide": {
+        "font": "0/0 a",
+        "color": "transparent",
+        "textShadow": "none",
+        "backgroundColor": "transparent",
+        "border": 0
+    },
+    "hidden": {
+        "display": "none !important",
+        "visibility": "hidden !important"
+    },
+    "affix": {
+        "position": "fixed",
+        "WebkitTransform": "translate3d(0, 0, 0)",
+        "transform": "translate3d(0, 0, 0)"
+    }
+});
